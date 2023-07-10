672c4a20f3c87553e704132b732a8c5c1f4d51cb
Muhammad Sameer <sameergb25@outlook.com>
2023-07-10 20:14:31 +0500

    update behavior for rugby ball pair

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -9,16 +9,20 @@ namespace DefaultNamespace
     {
         public static Action<string, Vector3> BallCollided;
         public        bool                    HasCollided = false;
-        
+
         private void OnCollisionEnter2D(Collision2D col)
         {
             if (HasCollided) return;
-            if (gameObject.name.Contains("Rugby")) return;
             if (!col.collider.CompareTag("Ball") || col.collider.gameObject.name != gameObject.name) return;
             HasCollided = true;
             col.collider.gameObject.GetComponent<Ball>()
                 .HasCollided = true;
-            BallCollided?.Invoke(gameObject.name, col.transform.position);
+
+            if (!gameObject.name.Contains("Rugby"))
+            {
+                BallCollided?.Invoke(gameObject.name, col.transform.position);
+            }
+
             Destroy(gameObject);
             Destroy(col.collider.gameObject);
         }
